d168d6e3585b4e73fe2703575212415d267c80e6
Kirilll13 <kirillsurtov@gmail.com>
2024-09-26 22:34:04 +1000

    4

---
+++ hw-2.js
@@ -0,0 +1,45 @@
+
+// task 1
+let a = 1020;
+alert(a);
+//task 2
+let ip = 2007;
+alert(ip);
+//task 3
+let name = `Брендан Эйх Ву`;
+alert(name);
+//task 4
+let ten = 10;
+let two = 2;
+alert(ten+two);
+alert(ten-two);
+alert(ten*two);
+alert(ten/two);
+//task 5
+let result = 2**5;
+alert(result);
+//task6
+let A = 9;
+let b = 2;
+alert(A/b);
+//task 7
+let num = 1;
+num+= 5;
+num-= 3;
+num*= 7;
+num/= 3;
+num++;
+num--;
+alert(num);
+//task 8
+let age = Number(prompt(`Сколько вам лет?`));
+alert(age);
+//task 9
+const user = {
+    name: 'Kirill',
+    age: 24,
+    "isAdmin": true
+};
+//task10
+let K = prompt(`Ваше имя?`);
+alert(`Привет, ${K}`);
